0859032816b7d361d9213c602ee6ed5b42e18df9
AlexZavodov <75357960+AlexZavodov@users.noreply.github.com>
2022-02-07 06:40:11 +0300

    Edit
    StorageCharacter Awake

--- Assets/Scripts/ItemStorageCharacter.cs
+++ Assets/Scripts/ItemStorageCharacter.cs
@@ -4,11 +4,6 @@ using UnityEngine;
 
 public class ItemStorageCharacter : ItemStorage
 {
-    private void Awake()
-    {
-        sizeStorageItems = 5;
-    }
-
     protected override Vector3 ItemPositionOffset(int intemCount)
     {
         return new Vector3(0, intemCount, 0);
